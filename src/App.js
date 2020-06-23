de63adc6f1c3f1a3ca71f181c078b70c68ca0b1c
Nicolás Carelli <nicolascarelli@gmail.com>
2020-06-23 19:11:27 -0300

    endpoint fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import { Route, Redirect, Switch } from "react-router-dom"
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Vinyls from "./components/vinyls"
+import VinylForm from "./components/vinyls/form"
+import NavBar from "./components/navBar"
+import { getVinyls } from "./services/vinylService"
+import { connect } from 'react-redux'
+import { setInitialVinyls } from './redux/actions/actions'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  async componentDidMount() {
+    const { data } = await getVinyls()
+    console.log("data", data)
+    this.props.setInitialVinyls(data)
+  }
+
+  render() {
+ 
+    return (
+      <div className="container-fluid">
+        <ToastContainer />
+        <NavBar />
+        <div className="container py-4">
+          <Switch>
+            <Route path="/vinyls/:id" component={VinylForm} />
+            <Route path="/vinyls" component={Vinyls} />
+            <Redirect from="/" exact to="/vinyls" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+const mapDispatchToProprs = {
+  setInitialVinyls
+}
+
+export default connect(null, mapDispatchToProprs)(App)
